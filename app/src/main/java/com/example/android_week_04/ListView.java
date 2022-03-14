e92efdb927aec90bc269594116dd0e9fd6eca5a4
nguyenthanhson162001 <nguyenthanhson162001@gmail.com>
2022-03-14 17:02:13 +0700

    'finish'

---
+++ app/src/main/java/com/example/android_week_04/ListView.java
@@ -0,0 +1,14 @@
+package com.example.android_week_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
